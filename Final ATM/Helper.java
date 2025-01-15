d2cc8bd40e0242da332f8bbfbf1ce61f93a1ec86
DarshanaMohan <darshhhh2006@gmail.com>
2025-01-15 15:45:36 +0530

    Add files via upload

---
+++ Final ATM/Helper.java
@@ -0,0 +1,7 @@
+public class Helper
+{
+        public static void main(String[] args) throws CloneNotSupportedException {
+            ATM.start();//to call  the start method with the help of atm class
+        }
+
+}
